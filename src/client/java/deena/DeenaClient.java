2f877f7977c732c541eefd262e3eed7133d4f7ef
Lee Clark <wymux@shoash.com>
2023-10-26 01:38:05 +0000

    Genesis

---
+++ src/client/java/deena/DeenaClient.java
@@ -0,0 +1,10 @@
+package deena;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeenaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}

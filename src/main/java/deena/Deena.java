aaffc68dba66297d63f131c341c03171b2d7770d
Lee Clark <wymux@shoash.com>
2023-11-03 03:09:37 +0000

    Initial fabric tutorial

--- src/main/java/deena/Deena.java
+++ src/main/java/deena/Deena.java
@@ -1,22 +1,23 @@
 package deena;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Deena implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("deena");
+	public static final Logger LOGGER = LoggerFactory.getLogger("deena");
+	public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
 	}
 }

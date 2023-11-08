ddc805d84002e5e07f23e3b9f29d64d5352fbf18
Lee Clark <wymux@shoash.com>
2023-11-08 12:07:23 +0000

    Add `custom_item`
    https://fabricmc.net/wiki/tutorial:items

--- src/main/java/deena/Deena.java
+++ src/main/java/deena/Deena.java
@@ -2,22 +2,38 @@ package deena;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.item.*;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import deena.DItem;
 
 public class Deena implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("deena");
-	public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
+	
+	public static final Item CUSTOM_ITEM =
+		Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"),
+				  new DItem(new FabricItemSettings().maxCount(16)));
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
+		FuelRegistry.INSTANCE.add(CUSTOM_ITEM, 300);
+		CompostingChanceRegistry.INSTANCE.add(CUSTOM_ITEM, 0.2F);
 	}
 }
+
+
+
+
+
+
+
+
+

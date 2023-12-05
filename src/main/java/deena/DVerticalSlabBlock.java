9d25fda68183ddec8679b9ecc317f3cb6973bc33
Lee Clark <wymux@shoash.com>
2023-12-05 01:09:51 +0000

    DVerticalSlabBlock: Waterlog
    - https://fabricmc.net/wiki/tutorial:waterloggable
    
    Signed-off-by: Lee Clark <wymux@shoash.com>

--- src/main/java/deena/DVerticalSlabBlock.java
+++ src/main/java/deena/DVerticalSlabBlock.java
@@ -5,25 +5,35 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.Waterloggable;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
 
-public class DVerticalSlabBlock extends HorizontalFacingBlock {
 
+public class DVerticalSlabBlock extends HorizontalFacingBlock implements Waterloggable {
+
+	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+	
 	public DVerticalSlabBlock(Settings settings) {
 		super(settings);
-		setDefaultState(getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+		setDefaultState(getDefaultState()
+				.with(Properties.HORIZONTAL_FACING, Direction.NORTH)
+				.with(WATERLOGGED, false));
 	}
 
 	@Override
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-		builder.add(Properties.HORIZONTAL_FACING);
+		builder.add(Properties.HORIZONTAL_FACING, WATERLOGGED);
 	}
 
 	@Override
@@ -46,7 +56,23 @@ public class DVerticalSlabBlock extends HorizontalFacingBlock {
 
 	@Override
 	public BlockState getPlacementState(ItemPlacementContext ctx) {
-		return super.getPlacementState(ctx).with(Properties.HORIZONTAL_FACING,
-							 ctx.getHorizontalPlayerFacing().getOpposite());
+		return (BlockState)this.getDefaultState()
+			.with(Properties.HORIZONTAL_FACING, ctx.getPlayerLookDirection().getOpposite())
+			.with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).isOf(Fluids.WATER));
+	}
+
+	@Override
+	public FluidState getFluidState(BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+	}
+
+	@Override
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState,
+						    WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+		if (state.get(WATERLOGGED)) {
+			world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+
+		return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
 	}
 }

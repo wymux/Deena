f95d7c208f93a0c74f3f0a897cca62d6deb05558
Lee Clark <wymux@shoash.com>
2023-12-09 01:06:21 +0000

    DBlock: Sync with Nbt
    - https://fabricmc.net/wiki/tutorial:blockentity_sync_itemstack
    
    Signed-off-by: Lee Clark <wymux@shoash.com>

--- src/main/java/deena/DBlock.java
+++ src/main/java/deena/DBlock.java
@@ -3,13 +3,20 @@ package deena;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+
+import java .util.List;
 
 public class DBlock extends Block {
 
@@ -31,5 +38,18 @@ public class DBlock extends Block {
 		}
 
 		return ActionResult.PASS;
+	}
+
+	@Override
+	public void appendTooltip(ItemStack stack, BlockView world, List<Text> tooltip, TooltipContext context) {
+		NbtCompound nbt = BlockItem.getBlockEntityNbt(stack);
+		if (nbt == null) {
+			NbtCompound nbt2 = new NbtCompound();
+			nbt2.putInt("number", 0);
+
+			BlockItem.setBlockEntityNbt(stack, Deena.DEMO_BLOCK_ENTITY, nbt2);
+		}
+
+		tooltip.add(Text.literal("Number: "+nbt.getInt("number")));
 	}
 }
